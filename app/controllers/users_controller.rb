afc056033be99aed0dfba9920f616c2b527755ea
hieuhlc <holechihieu@gmail.com>
2013-06-28 13:16:00 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
   end
   def new
   	@user = User.new
+  	flash[:success] = "Registration Succeeded! Welcome to VenShop :)"
+  		
+  end
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Registration Succeeded! Welcome to VenShop :)"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 
 end
